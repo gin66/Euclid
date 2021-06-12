32c6ecc8fc4c56b2487c2f43542f68605e860f15
Jochen Kiemes <jochen@kiemes.de>
2021-06-13 00:14:54 +0200

    First implementation, which has problem with subtract

--- Sources/Detector.swift
+++ Sources/Detector.swift
@@ -28,6 +28,10 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //  SOFTWARE.
 //
+private struct Edge: Hashable {
+    var p1: Vector
+    var p2: Vector
+}
 
 public extension Mesh {
     /// Returns a new mesh representing the combined volume of the
@@ -43,6 +47,75 @@ public extension Mesh {
     ///          +-------+            +-------+
     ///
     func detectSubMeshes() -> [Mesh] {
-        return [self]
+        var polygonToEdges: [Polygon: [Edge]] = [:]
+        var edgeToPolygons: [Edge: [Polygon]] = [:] // must be even number of polygons
+        
+        for polygon in polygons {
+            var edges: [Edge] = []
+            for i in 0 ..< polygon.vertices.count {
+                for j in i+1 ..< polygon.vertices.count {
+                    var p_i = polygon.vertices[i].position
+                    var p_j = polygon.vertices[j].position
+                    var swap = false
+                    if (p_i.x > p_j.x) {
+                        swap = true
+                    }
+                    else if (p_i.x == p_j.x) {
+                        if (p_i.y > p_j.y) {
+                            swap = true
+                        }
+                        else if (p_i.y == p_j.y) {
+                            if (p_i.z > p_j.z) {
+                                swap = true
+                            }
+                            if (p_i.z == p_j.z) {
+                                print("NOW WHAT ?")
+                            }
+                        }
+                    }
+                    if swap {
+                        let temp = p_i
+                        p_i = p_j
+                        p_j = temp
+                    }
+                    let e = Edge(p1: p_i, p2: p_j)
+                    edges.append(e)
+                    
+                    var polys = edgeToPolygons.removeValue(forKey: e) ?? []
+                    polys.append(polygon)
+                    edgeToPolygons[e] = polys
+                }
+                polygonToEdges[polygon] = edges
+            }
+        }
+        
+        var submeshes: [[Polygon]] = []
+        while let pair = edgeToPolygons.first{
+            var submesh: [Polygon] = []
+            var edgesOfSubmesh: Set<Edge> = []
+            var edgesToGo: Set<Edge> = []
+            
+            // Start with one random edge
+            edgesToGo.insert(pair.0)
+            while let edge = edgesToGo.first {
+                edgesToGo.remove(edge)
+                edgesOfSubmesh.insert(edge)
+                let polys = edgeToPolygons.removeValue(forKey: edge)
+                for poly in polys! {
+                    for e in polygonToEdges[poly]! {
+                        if !edgesOfSubmesh.contains(e) {
+                            edgesToGo.insert(e)
+                        }
+                    }
+                    submesh.append(poly)
+                }
+            }
+            submeshes.append(submesh)
+        }
+        var result: [Mesh] = []
+        for _ in submeshes {
+            result.append(self)
+        }
+        return result
     }
 }
--- Tests/DetectorTests.swift
+++ Tests/DetectorTests.swift
@@ -16,12 +16,61 @@ class DetectorTests: XCTestCase {
         let detected = a.detectSubMeshes()
         XCTAssert(detected.count == 1)
     }
+    func testCombinedMeshWithDistance() {
+        let a = Mesh.cube(size:1).translated(by: Vector(2,0,0))
+        let b = Mesh.cube(size:1)
+        let c = a.union(b)
+        let detected = c.detectSubMeshes()
+        XCTAssert(detected.count == 2)
+    }
+    func testCombinedMeshAttached() {
+        let a = Mesh.cube(size:1).translated(by: Vector(1,0,0))
+        let b = Mesh.cube(size:1)
+        let c = a.union(b)
+        let detected = c.detectSubMeshes()
+        XCTAssert(detected.count == 1)
+    }
 
-    func testSubtractCoincidingBoxesWhenTriangulated() {
+    func testDetectFourSubmeshes_0() {
+        let a = Mesh.cube(size:50)
+        let b = Mesh.cylinder(radius:30, height:60)
+        let c = a.subtract(b)
+        let detected = c.detectSubMeshes()
+        XCTAssert(detected.count == 0)
+    }
+    func testDetectFourSubmeshes_1() {
+        let a = Mesh.cube(size:50)
+        let b = Mesh.cylinder(radius:30, height:60)
+        let c = a.subtract(b)
+        let detected = c.detectSubMeshes()
+        XCTAssert(detected.count == 1)
+    }
+    func testDetectFourSubmeshes_2() {
+        let a = Mesh.cube(size:50)
+        let b = Mesh.cylinder(radius:30, height:60)
+        let c = a.subtract(b)
+        let detected = c.detectSubMeshes()
+        XCTAssert(detected.count == 2)
+    }
+    func testDetectFourSubmeshes_3() {
+        let a = Mesh.cube(size:50)
+        let b = Mesh.cylinder(radius:30, height:60)
+        let c = a.subtract(b)
+        let detected = c.detectSubMeshes()
+        XCTAssert(detected.count == 3)
+    }
+    func testDetectFourSubmeshes_4() {
         let a = Mesh.cube(size:50)
         let b = Mesh.cylinder(radius:30, height:60)
         let c = a.subtract(b)
         let detected = c.detectSubMeshes()
         XCTAssert(detected.count == 4)
+    }
+    func testDetectFourSubmeshes_big() {
+        let a = Mesh.cube(size:50)
+        let b = Mesh.cylinder(radius:30, height:60)
+        let c = a.subtract(b)
+        let detected = c.detectSubMeshes()
+        XCTAssert(detected.count > 4)
     }
 }
